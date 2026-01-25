4dabbd598cd3e088d404ccc855f16433c78b8264
miraclecolaboy <miraclecola4@gmail.com>
2026-01-25 16:12:48 +0800

    Add files via upload

--- supabase.config.js
+++ supabase.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   // postgresql://postgres:YOUR_DB_PASSWORD@db.<project-ref>.supabase.co:5432/postgres
-  databaseUrl: 'postgresql://postgres:K9ctAgpnh8XkJIT9@db.fjjbodkvytpekzzxerzr.supabase.co:5432/postgres',
+  databaseUrl: 'postgresql://postgres.fjjbodkvytpekzzxerzr:K9ctAgpnh8XkJIT9@aws-1-us-east-1.pooler.supabase.com:5432/postgres',
   ssl: { rejectUnauthorized: false }
 };
