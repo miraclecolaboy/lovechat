c345dbf7b94d2a571e6295c724fa385fbde61ebe
miraclecolaboy <miraclecola4@gmail.com>
2026-02-12 13:23:55 +0800

    Delete supabase.config.js

--- supabase.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // postgresql://postgres:YOUR_DB_PASSWORD@db.<project-ref>.supabase.co:5432/postgres
-  databaseUrl: 'postgresql://postgres.fjjbodkvytpekzzxerzr:L8XPsCdDyqRjavnh@aws-1-us-east-1.pooler.supabase.com:5432/postgres',
-  ssl: { rejectUnauthorized: false }
-};
